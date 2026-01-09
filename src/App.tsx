2d3577e0e7f7f328d37df2dd023a6a9cb2f2e5e3
Musfiq Rahman <procreationsbliss@gmail.com>
2026-01-09 12:12:01 +0600

    Initial setup of the project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-amber-700">Home page</h1>
+      <Button>Click Me</Button>
+    </>
+  );
+}
+
+export default App;
