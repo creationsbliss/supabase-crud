10c1dfdf64c6a86bc1319b67f566c58dfd3d08b4
Musfiq Rahman <procreationsbliss@gmail.com>
2026-01-09 21:18:12 +0600

    Implement delete operations

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -4,6 +4,9 @@ import {
   PencilEdit02Icon as EditIcon,
 } from "@hugeicons/core-free-icons";
 
+import supabase from "./supabase-client";
+import { useEffect, useState } from "react";
+
 import {
   Table,
   TableBody,
@@ -13,26 +16,54 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { Button } from "./components/ui/button";
+import { toast } from "sonner";
 
-const invoices = [
-  {
-    invoice: "INV001",
-    paymentStatus: "Paid",
-    paymentMethod: "Credit Card",
-  },
-  {
-    invoice: "INV002",
-    paymentStatus: "Pending",
-    paymentMethod: "PayPal",
-  },
-  {
-    invoice: "INV003",
-    paymentStatus: "Unpaid",
-    paymentMethod: "Bank Transfer",
-  },
-];
+interface TodoListsShpape {
+  id: number;
+  created_at: string;
+  name: string;
+  isCompleted: boolean;
+}
 
 const TodoList = () => {
+  const [toDoLists, setToDoLists] = useState<TodoListsShpape[]>([]);
+
+  // Fetch todo list from Supabase
+  useEffect(() => {
+    const fetchToDos = async () => {
+      const { data, error } = await supabase.from("TodoList").select();
+
+      if (error) {
+        toast.error("Failed to fetch todos. Please try again.");
+        return;
+      }
+
+      setToDoLists(data);
+    };
+
+    fetchToDos();
+  }, [toDoLists]);
+
+  // Handle delete todo
+  const handleDelete = async (id: number) => {
+    const { error } = await supabase.from("TodoList").delete().eq("id", id);
+
+    if (error) {
+      toast.error("Failed to delete todo. Please try again.");
+    } else {
+      toast.success("Todo deleted successfully!");
+      setToDoLists((prev) => prev.filter((todo) => todo.id !== id));
+    }
+  };
+
+  if (!toDoLists) {
+    return <p className="pt-10 text-center">Loading todos...</p>;
+  }
+
+  if (toDoLists.length === 0) {
+    return <p className="pt-10 text-center">No todos found.</p>;
+  }
+
   return (
     <div className="pt-10">
       <Table>
@@ -44,15 +75,19 @@ const TodoList = () => {
           </TableRow>
         </TableHeader>
         <TableBody>
-          {invoices.map((invoice) => (
-            <TableRow key={invoice.invoice}>
-              <TableCell className="font-medium">{invoice.invoice}</TableCell>
-              <TableCell>{invoice.paymentStatus}</TableCell>
+          {toDoLists.map(({ id, name, isCompleted }) => (
+            <TableRow key={id}>
+              <TableCell>{name}</TableCell>
+              <TableCell>{isCompleted ? "Completed" : "Pending"}</TableCell>
               <TableCell className="flex gap-2 justify-end">
-                <Button variant="outline">
+                <Button variant="outline" className="cursor-pointer">
                   <HugeiconsIcon icon={EditIcon} />
                 </Button>
-                <Button variant="destructive">
+                <Button
+                  onClick={() => handleDelete(id)}
+                  variant="destructive"
+                  className="cursor-pointer"
+                >
                   <HugeiconsIcon icon={DeleteIcon} />
                 </Button>
               </TableCell>
