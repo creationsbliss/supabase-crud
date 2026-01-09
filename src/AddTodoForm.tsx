33d5c33427b0026cb30d94010a0c603d0104ca24
Musfiq Rahman <procreationsbliss@gmail.com>
2026-01-09 19:46:27 +0600

    Add insert functionality

--- src/AddTodoForm.tsx
+++ src/AddTodoForm.tsx
@@ -1,11 +1,46 @@
+import { useState } from "react";
+import supabase from "./supabase-client";
+
 import { Button } from "./components/ui/button";
 import { Input } from "./components/ui/input";
+import { toast } from "sonner";
 
 const AddTodoForm = () => {
+  const [toDoItem, setDoToItem] = useState("");
+  const [addToDo, setAddToDo] = useState(false);
+
+  const handleAddToDo = async () => {
+    setAddToDo(true);
+
+    const newTodo = {
+      name: toDoItem,
+      isCompleted: false,
+    };
+
+    const { error } = await supabase.from("TodoList").insert(newTodo);
+
+    if (error) {
+      toast.error("Failed to add todo. Please try again.");
+    } else {
+      toast.success("Todo added successfully!");
+    }
+
+    setDoToItem("");
+    setAddToDo(false);
+  };
+
   return (
     <div className="flex gap-2">
-      <Input type="text" placeholder="Add a new todo..." />
-      <Button>Add</Button>
+      <Input
+        type="text"
+        placeholder="Add a new todo..."
+        value={toDoItem}
+        onChange={(e) => setDoToItem(e.target.value)}
+      />
+
+      <Button onClick={handleAddToDo} disabled={addToDo}>
+        {addToDo ? "Adding..." : "Add"}
+      </Button>
     </div>
   );
 };
